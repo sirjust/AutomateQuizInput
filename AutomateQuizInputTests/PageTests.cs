bf995d82711daabe0a3ac1ede5f8ef9961a7d568
SirJUST <theoperaking@yahoo.com>
2019-07-17 10:12:38 -0400

    Add preliminary page tests, update pageContainer name

---
+++ AutomateQuizInputTests/PageTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AutomateQuizInput;
+
+namespace AutomateQuizInputTests
+{
+    [TestClass]
+    public class PageTests
+    {
+        [TestMethod]
+        public void GetPages_ShouldThrowException_WhenQuizzesAndPagesUnequal()
+        {
+            // arrange
+            var quizzes = new List<Quiz>();
+            var pages = TestAuxiliaryMethods.GetMockPageInputData();
+            PageContainer pageContainer = new PageContainer();
+
+            // act
+
+            // assert
+            Assert.ThrowsException<Exception>(pageContainer.GetPages(pages, quizzes.Count));
+        }
+
+        [TestMethod]
+        public void InsertPages_ShouldThrowException_WhenCountOfQuizListAndPageObjectsUnequal()
+        {
+
+        }
+    }
+}
