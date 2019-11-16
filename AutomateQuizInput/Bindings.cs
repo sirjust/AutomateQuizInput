6733a26d271ccd3a743474ab9154e6d3eb2f79eb
SirJUST <theoperaking@yahoo.com>
2019-11-15 23:22:15 -0500

    Use Ninject for initial dependency injection

---
+++ AutomateQuizInput/Bindings.cs
@@ -0,0 +1,22 @@
+﻿using Ninject.Modules;
+using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomateQuizInput
+{
+    public class Bindings : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IReader>().To<Reader>();
+            Bind<IQuizBuilder>().To<QuizBuilder>();
+            Bind<IPageContainer>().To<PageContainer>();
+            Bind<ITextChecker>().To<TextChecker>();
+            Bind<IUploader>().To<Uploader>();
+        }
+    }
+}
--- AutomateQuizInput/Program.cs
+++ AutomateQuizInput/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using Ninject;
 
 namespace AutomateQuizInput
 {
@@ -10,6 +12,15 @@ namespace AutomateQuizInput
     {
         static void Main(string[] args)
         {
+            // get dependencies
+            var kernel = new StandardKernel();
+            kernel.Load(Assembly.GetExecutingAssembly());
+            var reader = kernel.Get<IReader>();
+            var builder = kernel.Get<IQuizBuilder>();
+            var checker = kernel.Get<ITextChecker>();
+            var uploader = kernel.Get<IUploader>();
+            var pageContainer = kernel.Get<IPageContainer>();
+
             // the text document needs to be in the Docs folder and in the right format
             Console.WriteLine("This program will automatically input quizzes into the AnytimeCE Admin UI.\nFirst we will go through the provided text documents.");
             Console.WriteLine("What is the Course Id?\n ***IMPORTANT*** This must match an available course in the portal.");
@@ -17,11 +28,6 @@ namespace AutomateQuizInput
             string path = @"../../Docs/Quizzes.txt";
 
             Console.WriteLine("We will now check the document for invalid characters, such as the single quote.");
-
-            IReader reader = new Reader();
-            IQuizBuilder builder = new QuizBuilder();
-            ITextChecker checker = new TextChecker();
-            IUploader uploader = new Uploader();
 
             var allLines = reader.ReadDocument(path);
             var allLinesList = checker.FindAndReplaceInvalidCharacters(allLines).ToList();
@@ -41,7 +47,6 @@ namespace AutomateQuizInput
             }
 
             // add three page numbers to each quiz using the PageInfo document
-            PageContainer pageContainer = new PageContainer(builder);
             var pageDocLines = reader.ReadDocument(@"../../Docs/PageInfo.txt");
             var pages = pageContainer.GetPages(pageDocLines, completeQuizzes.Count());
             pageContainer.InsertPages(completeQuizzes, pages.ToList());
